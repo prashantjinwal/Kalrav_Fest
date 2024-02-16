909e6d05042e3e25a2dcb159af227da59ea3ec19
prashantjinwal <prashnatjinwal888@gmail.com>
2024-02-16 14:20:54 +0530

    init

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='bg-black text-white'>test</p>
+    </>
+  )
+}
+
+export default App
