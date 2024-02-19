10c00484c7b5ca872a42aa71fab8f12b55bddffd
Prashant Jinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-02-19 15:03:56 +0530

    Init Commit

---
+++ Kalrav-fest/src/assets/Team.js
@@ -0,0 +1,25 @@
+const Team1 = [
+    {
+        name:"fdndfj",
+        src:"/images/home/team.png"
+    },
+    {
+        name:"dffds",
+        src:"/images/home/team.png"
+    },
+    {
+        name:"dsaf",
+        src:"/images/home/team.png"
+    },
+    {
+        name:"fffsa",
+        src:"/images/home/team.png"
+    },
+    {
+        name:"rggda",
+        src:"/images/home/team.png"
+    },
+];
+
+
+export default Team1;
