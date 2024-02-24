ce1b62834b4da277a34668d02046e437109d2a75
Prashant Jinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-02-24 21:16:41 +0530

    ADDED

---
+++ src/assets/Team.js
@@ -0,0 +1,104 @@
+const Team1 = [
+    {
+        name:"Harsh Chauhan",
+        src:"/images/Team/cu01.jpeg"
+    },
+    // {
+    //     name:"dffds",
+    //     src:"/images/Team/cu02.jpeg"
+    // },
+    {
+        name:"Ayush Jain",
+        src:"/images/Team/cu03.jpeg"
+    },
+    
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/cu05.jpeg"
+    // },
+    {
+        name:"Nalin Yadav",
+        src:"/images/Team/cu06.jpeg"
+    },
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/cu07.jpeg"
+    // },
+    
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/cu09.jpeg"
+    // },
+   
+    {
+        name:"Rakshita",
+        src:"/images/Team/cu12.jpeg"
+    },
+    {
+        name:"Saraswati Thapa",
+        src:"/images/Team/cu04.jpeg"
+    },
+];
+const Coordinaters = [
+    {
+        name:"Bharat Kaushik",
+        src:"/images/Team/coor01.jpeg"
+    },
+    {
+        name:"Yash Chandola",
+        src:"/images/Team/coor02.jpeg"
+    },
+    // {
+    //     name:"dsaf",
+    //     src:"/images/Team/coor03.jpeg"
+    // },
+    // {
+    //     name:"fffsa",
+    //     src:"/images/Team/coor04.jpeg"
+    // },
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/coor05.jpeg"
+    // },
+    {
+        name:"Mayank Suri",
+        src:"/images/Team/coor06.jpeg"
+    },
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/coor07.jpeg"
+    // },
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/coor08.jpeg"
+    // },
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/coor09.jpeg"
+    // },
+    // {
+    //     name:"rggda",
+    //     src:"/images/Team/coor10.jpeg"
+    // },
+    {
+        name:"Sanket Pathak",
+        src:"/images/Team/coor11.jpeg"
+    },
+    {
+        name:"Aryan Pradhan",
+        src:"/images/Team/coor12.jpeg"
+    },
+    
+    {
+        name:"Palak Tiwari",
+        src:"/images/Team/cu10.jpeg"
+    },
+    {
+        name:"Anveshika Bhat",
+        src:"/images/Team/cu08.jpeg"
+    },
+];
+
+
+export default Team1;
+export {Coordinaters};
