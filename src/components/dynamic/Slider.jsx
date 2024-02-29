9b5664ccde971c2584beb7eeeb1fa8a1078ab018
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-02-29 21:27:35 +0530

    Update Silder

--- src/components/dynamic/Slider.jsx
+++ src/components/dynamic/Slider.jsx
@@ -8,7 +8,8 @@ function Slider(props){
 
         <div className=" flex gap-3 justify-center w-full ">
 
-           <div> <img  className="h-[10em] rounded-[100em] bg-black border-4 border-[#BE9D6C] min-w-[9em] lg:min-w-[10em] lg:h-[10em] py-2" src={process.env.PUBLIC_URL + props.src} alt="" />
+           <div className="lg:mx-2"> 
+            <img  className="h-[9em] rounded-full bg-black border-4 border-[#BE9D6C] min-w-[9em] lg:min-w-[11em] lg:h-[11em] " src={process.env.PUBLIC_URL + props.src} alt="#" />
             <p className="TeamT text-white font-bold text-base underline decoration-1 py-1 flex justify-center ">{props.name}</p></div>
             
         </div>
