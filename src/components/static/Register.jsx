3f391ecef495b78d5afba8c682ca1c14b1c0b29a
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-03-05 00:46:05 +0530

    Add Registertion page

---
+++ src/components/static/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <>
+       <button className="text-white text-base btn-res  hover:bg-[#f9b550] font-mono font-semibold rounded-full px-5 lg:px-4 py-2 bg-[#cda160]">
+													REGISTER
+		</button>	
+    </>
+  )
+}
+
+export default Register;
