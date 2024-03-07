26095238d94b50e4a4b54b7f4fd07be1ef7a7e0b
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-03-08 00:16:31 +0530

    Resgistation PageUp-1

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./resgister.css"
 
 function Register() {
@@ -7,7 +8,7 @@ function Register() {
     
     <section className='main-body px-4 py-7 lg:py-9'>
         {/* temp.. wartning */}
-    <h3 className=' text-red-600 font-semibold text-2xl py-3 mb-5' > This page updated Soon.. </h3>
+    <h3 className=' text-red-600 font-semibold text-2xl py-3 mb-5' > This page will updated Soon.. </h3>
 
     <div className='headings flex justify-center'>
         <h3 className=' text-white text-4xl lg:text-5xl font-bold '> Click On The Below Section To Register DAY-WISE For <span className=' font-bold text-yellow-600'>KALRAV</span> 24 Passes </h3>
@@ -22,10 +23,26 @@ function Register() {
 
             {/* Day wise button  */}
 
-        <div className='btn text-white grid grid-rows-3 gap-7 mx-12'>
-            <button className='py-3 rounded-xl  text-4xl font-semibold bg-[#BE9D6C] '> DAY 1</button>
-            <button className='py-3 rounded-xl text-4xl font-semibold bg-[#BE9D6C]' > DAY 2</button>
-            <button className='py-3 rounded-xl text-4xl font-semibold bg-[#BE9D6C]' > DAY 3</button>
+        <div className='btn text-white flex justify-center gap-10 flex-col mx-12'>
+
+          <div className='flex justify-center'>
+          <Link to="/Register/Day" key="Day1">
+            <button className='p-3 rounded-md  text-4xl font-semibold bg-[#BE9D6C] border-b-2 border-r-2 border-solid border-yellow-800 '> DAY 1</button>
+          </Link>
+          </div>
+
+          <div className='flex justify-center'>
+          <Link to="/Register/Day2" key="Day2Passes">
+            <button className='p-3 rounded-md text-4xl font-semibold bg-[#BE9D6C] border-b-2 border-r-2 border-solid border-yellow-800 ' > DAY 2</button>
+          </Link>
+          </div>
+
+          <div className='flex justify-center'>
+          <Link to="/Register/Day3" key="Day3Passes">
+            <button className='p-3 rounded-md text-4xl font-semibold bg-[#BE9D6C] border-b-2 border-r-2 border-solid border-yellow-800 ' > DAY 3</button>
+          </Link>
+          </div>
+
         </div>
 
     </section>
