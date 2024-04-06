6400c1e358a986a39b18bba687b5ebf2af174a50
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-04-06 14:32:29 +0530

    Update Compi page #8

--- src/components/dynamic/Compi.jsx
+++ src/components/dynamic/Compi.jsx
@@ -12,7 +12,7 @@ function Compscard(props){
             <img className=" lg:w-[24em] w-[14em]  gap-3 border border-white border-dashed " src={process.env.PUBLIC_URL + props.src} alt="#" />
         </div>
         <div className="flex justify-center mt-4">
-        <a href={props.link}> <button className= "comptextheader font-bold text-2xl text-white flex justify-center px-5 py-2 mb-2 rounded-lg bg-[#BE9D6C]"> Register </button></a>
+        <a href={props.link}> <button className= "comptextheader font-bold text-2xl text-white flex justify-center px-5 py-2 mb-2 rounded-lg bg-gradient-to-r from-[#582f0e] to-[#e2bc2f] border border-white border-solid "> Register </button></a>
         </div>
     </div>
        
