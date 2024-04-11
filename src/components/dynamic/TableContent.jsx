cb69243625d7c357cda6024c552e7317e368c69e
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-04-11 15:08:16 +0530

    Add Table Loader

--- src/components/dynamic/TableContent.jsx
+++ src/components/dynamic/TableContent.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TableContent(props) {
+  const [LoaderTable , setLoaderTable] = useState(true);
+
+  setTimeout(function(){setLoaderTable(false)},2000)
+
   return (
     <>
       
 
         <div className=" flex justify-center  flex-col">
-            <img className=' lg:w-[20em] flex justify-center border border-white border-double ' src={props.src} alt="techical issue" />
+          {!LoaderTable ?   <img className=' lg:w-[20em] flex justify-center border border-white border-double ' src={props.src} alt="techical issue" />  : <div className="Competiton-poster-loder rounded-lg mt-4 mx-[2em] lg:h-[32em] lg:w-[20em] h-[25em] bg-black"> </div> }
            <a href={props.src} className=' flex justify-center py-4'  download ><button className='btn-style text-white py-4 px-2 rounded-md  comptextheader font-bold  border border-white border-double'>{props.title}</button> </a>
         </div>
 
