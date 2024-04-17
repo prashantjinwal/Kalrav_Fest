78d5fb447ba378182e9395d2bdf9f1d8f24ae40c
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-04-17 22:34:28 +0530

    Update-Table

--- src/assets/table.js
+++ src/assets/table.js
@@ -1,17 +1,17 @@
  const table = [
     {
-        src : "images/itnarery/table/day1-tableUpdate.jpeg",
+        src : "images/itnarery/table/day1.jpeg",
         title: " Click here to download Day-1",
         key:1,
         
     },
     {
-        src : "images/itnarery/table/day2-tableUpdate.jpeg",
+        src : "images/itnarery/table/day2.jpeg",
         title: "Click here to download Day-2",
         key:2,
     },
     {
-        src : "images/itnarery/table/day3-tableUpdate.jpeg",
+        src : "images/itnarery/table/day3.jpeg",
         title: "Click here to download Day-3",
         key:3,
     }
